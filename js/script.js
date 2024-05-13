1eb5116753caac9b8f857dee36dd60e53366407c
Pavel <example@gmail.com>
2024-05-14 01:15:28 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// let storeName = "Istore"
+// let storeDescription = {
+//     budget: 10000,
+//     employees: [
+//         'Alex', 'Sid', 'Roy'
+//     ],
+//     products: {
+//         first: 1000,
+//         second: 2000
+//     },
+//     open: true
+// }
+
+const answers = []
+answers[0] = prompt('Как зовут?')
+answers[1] = prompt('Как фамилия?')
+answers[2] = +prompt('Сколтко лет?')
+console.log(answers);
